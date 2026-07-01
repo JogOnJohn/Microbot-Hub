3100c7979b0f57dd4eb1894da88cedfc93605567
Billy <billy@example.local>
2026-07-01 19:10:49 +1000

    fix: remove duplicate house tab lectern id

--- src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
@@ -31,7 +31,7 @@ import java.awt.*;
 )
 @Slf4j
 public class HouseTabPlugin extends Plugin {
-    public static final String version = "1.0.53";
+    public static final String version = "1.0.54";
 
     @Inject
     private HouseTabConfig config;
