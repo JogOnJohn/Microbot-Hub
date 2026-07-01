a9446520c2f9f1dde08ad222ce3dfdfb7c4bf014
JogOnJohn <223066319+JogOnJohn@users.noreply.github.com>
2026-07-01 15:24:29 +1000

    fix: avoid slow progressive routing checks

--- src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
@@ -31,7 +31,7 @@ import java.awt.*;
 )
 @Slf4j
 public class HouseTabPlugin extends Plugin {
-    public static final String version = "1.0.32";
+    public static final String version = "1.0.33";
 
     @Inject
     private HouseTabConfig config;
