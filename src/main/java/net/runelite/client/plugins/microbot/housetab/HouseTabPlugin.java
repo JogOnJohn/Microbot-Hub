2ef5095207f661143c1cac9121bfe121643f94f5
JogOnJohn <223066319+JogOnJohn@users.noreply.github.com>
2026-07-02 13:27:48 +1000

    refactor: share progressive house exit handling

--- src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/housetab/HouseTabPlugin.java
@@ -31,7 +31,7 @@ import java.awt.*;
 )
 @Slf4j
 public class HouseTabPlugin extends Plugin {
-    public static final String version = "1.0.58";
+    public static final String version = "1.0.59";
 
     @Inject
     private HouseTabConfig config;
